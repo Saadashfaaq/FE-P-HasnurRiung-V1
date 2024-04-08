78093c80290e881445427405de1fa842a7ad7406
saadashfaaq <Reynaldysptian@gmail.com>
2024-04-08 11:32:03 +0700

    - [FEPHRV] Setup Branch

--- FEP-hasnurRiung-v1/src/app/modules/landing/form-leave/form-leave/form-leave.component.ts
+++ FEP-hasnurRiung-v1/src/app/modules/landing/form-leave/form-leave/form-leave.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-leave',
@@ -7,6 +8,68 @@ import { Component } from '@angular/core';
   templateUrl: './form-leave.component.html',
   styleUrl: './form-leave.component.scss'
 })
-export class FormLeaveComponent {
+export class FormLeaveComponent implements OnInit {
 
+  constructor(
+    private _formBuilder: UntypedFormBuilder,
+  ){}
+
+  // Varibale to keep form Leave
+  formLeaveSection1 : UntypedFormGroup
+  formLeaveSection2 : UntypedFormGroup
+  formLeaveSection3 : UntypedFormGroup
+
+
+    // Variable For Item List
+    categoryList = [
+      {
+        name:'Cuti',
+        value: 'cuti'
+      },
+      {
+        name:'Izin',
+        value: 'izin'
+      },
+      {
+        name:'Kompensasi',
+        value: 'kompensasi'
+      }
+    ]
+
+  ngOnInit(): void {
+    this.InitFormLeaveSection1()
+    this.InitFormLeaveSection2()
+    this.InitFormLeaveSection3()
+  }
+
+
+  InitFormLeaveSection1(){
+    this.formLeaveSection1 = this._formBuilder.group({
+      category: ['', [Validators.required]],
+      name: [''],
+      employee_number: [''],
+      family_status: [''],
+      date_of_employment: [''],
+      leave_location : ['', [Validators.required]],
+      leave_category:[''],
+      phone_number: ['', [Validators.required]],
+      is_ticket_supported: ['', [Validators.required]],
+      poh_location: ['',[Validators.required]],
+      position: ['', [Validators.required]],
+      poh_status: ['',[Validators.required]],
+      is_with_family: ['', [Validators.required]],
+      is_routine_official_letter: ['', [Validators.required]],
+      leave_address: ['', [Validators.required]],
+      is_lump_sump: ['', [Validators.required]],
+      lump_sump_amount: ['', [Validators.required]],
+    })
+  }
+
+  InitFormLeaveSection2(){
+
+  }
+  
+  InitFormLeaveSection3(){
+
+  }
 }
